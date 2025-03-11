2ecd5d95a2c396bd0118aa26c80ce9f4aaddba69
Mahmoud Mohamed <Mahmoud_2001M@outlook.com>
2025-03-11 09:28:14 +0400

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 # Update with your actual DB credentials
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dubai_to_the_stars"
+DATABASE_URL = "postgresql://test_xzio_user:izxgtvCyWZOUt6fIg0FYyln3wLCzbGOd@dpg-cv7sin9c1ekc73d1rufg-a.oregon-postgres.render.com/test_xzio"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
